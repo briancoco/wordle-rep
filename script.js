cef756df83d6350deb4b0e4da5bda9cc31a588b9
Brian Nguyen <briannguyencoco@gmail.com>
2022-07-10 17:12:26 -0500

    added matchingLetters() function to check for matching letters in each attempt

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 //Array variable that holds a lot of 5 letter words.
 let words = ['HELLO', 'BROWN', 'CRAZY', 'QUICK', 'JIGGY'];
 
-//Counter variable to hold the # of attempts thus far
-let attempts = 0;
 
 //Randomly select a word and prompt the user to guess the word. 
 let randWord = words[Math.floor(Math.random()*words.length)];
@@ -11,6 +9,7 @@ let guess = prompt('Enter your guess: ').toUpperCase();
 //If the guess is wrong, up the counter and ask user to guess again
 for(let i = 0; i < 6; i++) {
     if(guess != randWord) {
+        matchingLetters(randWord, guess);
         console.log(guess + ' is wrong guess, try again');
         guess = prompt('Enter your guess: ').toUpperCase();
     }
@@ -18,9 +17,39 @@ for(let i = 0; i < 6; i++) {
         console.log(guess + ' is correct, you win!');
     }
 }
+//if still havent guessed, and no more tries then return this
 if(guess != randWord) {
     console.log('out of tries, play again');
 }
 
-//If right, break; 
-//If user cannot guess in 6 tries, end game
+
+function matchingLetters(randWord, guess) {
+    //Variable that holds matching letters in the wrong place
+    //Variable that holds matching letters in the right place
+    let matchWrongPlace = [];
+    let matchRightPlace = [];
+
+    //For each letter in the attempt, see if it is in the word you’re trying to guess.
+    //If it is
+    //Check if the letter is in the right place, if it is then add to var holding right places
+    //else Add to var holding wrong places
+    for(let i = 0; i < 6; i++) {
+        if(randWord.includes(guess[i])) {
+            if(guess[i] === randWord[i]) {
+                matchRightPlace.push(guess[i]);
+            }
+            else {
+                matchWrongPlace.push(guess[i]);
+            }
+        }
+    }
+
+    //turns array results into a comma seperated string
+    let wrongString = matchWrongPlace.join(', ');
+    let rightString = matchRightPlace.join(', ');
+
+    //prints to console the results
+    console.log('Right letters, right place: ' + rightString);
+    console.log('Right letters, wrong place: ' + wrongString);
+}
+
