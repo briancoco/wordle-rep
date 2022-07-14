26a9b3d072e2e75f918e06a2a9d97cf6937bf3a6
Brian Nguyen <briannguyencoco@gmail.com>
2022-07-13 21:02:17 -0500

    fixed bug for backspacing on empty row

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ let win = false;
 //Randomly select a word and prompt the user to guess the word. 
 let randWord = words[Math.floor(Math.random()*words.length)];
 
+//object that holds game data 
 let stats = {
+    //holds current box values
     board : Array(6)
         .fill()
         .map(() => Array(5).fill('')),
-    row : 0,
-    column : 0,
+    row : 0,//current row
+    column : 0,//current column
 }
 
 const body = document.querySelector('body');
@@ -17,7 +19,7 @@ const container = document.querySelector('.container');
 let letter;
 
 
-
+//updates board by iteration through all boxes changing their text content according to game data
 function updateBoard() {
     for(let i = 0; i < 6; i++){
         for(let j = 0; j < 5; j++){
@@ -26,12 +28,16 @@ function updateBoard() {
         }
     }
 }
+//creates 5 x 6 grid for the wordle game
 function createBoard() {
     for(let i = 0; i < 6; i++) {
 
         for(let j = 0; j < 5; j++) {
+            
             const div = document.createElement('div');
             div.classList.add('box');
+            //each box is given a specific class based on it's position in the grid
+            //so we can reference each individual box
             div.classList.add(`b${i}${j}`);
             container.appendChild(div);
         }
@@ -39,15 +45,20 @@ function createBoard() {
 }
 
 function winner(board) {
+    //combines letters in the current row into word
     let word = board.join('');
+    //iterates over each letter in the current row checking for conditions
     for(let i = 0; i < 5; i++) {
         const div = document.querySelector(`.b${stats.row}${i}`);
+        //if letter matches and is in the right position, add matching styling
         if(word[i] == randWord[i]) {
             div.classList.add('match');
         }
+        //if letter matches and is in the wrong position, add wrong-place styling
         else if(randWord.includes(word[i])) {
             div.classList.add('wrong-place');
         }
+        //if no match add indicated styling
         else {
             div.classList.add('no-match');
         }
@@ -63,24 +74,31 @@ function winner(board) {
 createBoard();
 
 body.addEventListener('keydown', (e) => {
-    console.log(e);
+    //deletes last inputted letter from row
     if(e.key == 'Backspace') {
-        stats.column--;
-        stats.board[stats.row][stats.column] = '';
-        updateBoard();
+        if(stats.column > 0) {
+            stats.column--;
+            stats.board[stats.row][stats.column] = '';
+            updateBoard();
+        }
+        
     }
-    if(e.code.includes('Key')){
+    //if valid letter then add to board
+    if(e.code.includes('Key') ){
         letter = e.key.toUpperCase();
+        //checks if the row is full and changes if it is
         if(stats.column == 5) {
             stats.column = 0;
             stats.row++;
         }
+        //adds letter to game data and updates board if no winner
         if(!win) {
             stats.board[stats.row][stats.column] = letter;
             stats.column++;
     
             updateBoard();
         }
+        //checks for winner and stops game from running if so
         if(stats.column == 5) {
             win = winner(stats.board[stats.row]);
             
